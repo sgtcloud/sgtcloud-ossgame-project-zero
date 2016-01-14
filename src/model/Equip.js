29ebe78413c6d82000564718b9f92c9c901166f1
malong <malong@yoedge.com>
2016-01-14 13:38:02 +0800

    update hero equip and skill bizlogic

--- src/model/Equip.js
+++ src/model/Equip.js
@@ -2,62 +2,64 @@ var Equip = function (id, lv) {
     var id = id;
     var lv = lv;
     var data = dataSource.equips[id];
-    return {
-        getId: function () {
-            return id;
-        },
-        getLv: function () {
-            return lv;
-        },
-        getName: function () {
-            return data.name;
-        },
-        getDesc: function () {
-            return data.desc;
-        },
-        getLife: function () {
-            return data.life_base + data.life_grow * (lv - 1);
-        },
-        getAttack: function () {
-            return data.attack_base + data.attack_grow * (lv - 1);
-        },
-        getHit: function () {
-            return data.hit_base + data.hit_grow * (lv - 1);
-        },
-        getPrice: function () {
-            return data.price_base + data.price_grow * (lv - 1);
-        },
-        getEffect: function (key) {
-            return getEffectValue(data, key, lv);
-        },
-        upgrade: function (hero) {
-            var price = this.getPrice();
-            if (player.gold >= price) {
-                lv += 1;
-                PlayerData.consumeResource(PlayerData.createResourceData("gold", -price));
-                game.onEquipUpdate(hero, this);
-                game.onHeroUpdate(hero);
-            }
-        },
-
-        getLevelData: function (level) {
-            return getSpecificLevelData(data, level || lv);
-        },
-        traverseEquipEffects: function (lv) {
-            var equip = this.getLevelData(lv);
-            var effects = [];
-            for (var key in equip) {
-                //狗日的微信浏览器中没有startsWith
-                if (key.indexOf("effect") == 0) {
-                    var effect = equip[key];
-                    effects.push(effect);
-                    effect.index = parseInt(key.replace("effect", ""));
-                }
-            }
-            return effects.sort(function (a, b) {
-                return a.index - b.index;
-            });
+    this.getId = function () {
+        return id;
+    };
+    this.getLv = function () {
+        return lv;
+    };
+    this.getName = function () {
+        return data.name;
+    };
+    this.getIcon=function(){
+        return data.icon;
+    }
+    this.getDesc = function () {
+        return data.desc;
+    };
+    this.getLife = function () {
+        return data.life_base + data.life_grow * (lv - 1);
+    };
+    this.getAttack = function () {
+        return data.attack_base + data.attack_grow * (lv - 1);
+    };
+    this.getHit = function () {
+        return data.hit_base + data.hit_grow * (lv - 1);
+    };
+    this.getPrice = function () {
+        return data.price_base + data.price_grow * (lv - 1);
+    };
+    this.getEffect = function (key) {
+        return getEffectValue(data, key, lv);
+    };
+    this.upgrade = function (hero) {
+        var price = this.getPrice();
+        if (player.gold >= price) {
+            lv += 1;
+            PlayerData.consumeResource(PlayerData.createResourceData("gold", -price));
+            game.onEquipUpdate(hero, this);
+            game.onHeroUpdate(hero);
         }
     };
+
+    this.getLevelData = function (level) {
+        return getSpecificLevelData(data, level || lv);
+    };
+    this.traverseEquipEffects = function (lv) {
+        var equip = this.getLevelData(lv);
+        var effects = [];
+        for (var key in equip) {
+            //狗日的微信浏览器中没有startsWith
+            if (key.indexOf("effect") == 0) {
+                var effect = equip[key];
+                effect['name'] = key;
+                effects.push(effect);
+                effect.index = parseInt(key.replace("effect", ""));
+            }
+        }
+        return effects.sort(function (a, b) {
+            return a.index - b.index;
+        });
+    }
 };
 
