938ddf1fd04843ffa7fe66a1bf6c5639f0b58fcd
malong <malong@yoedge.com>
2016-01-18 19:16:55 +0800

    合并

--- src/model/SkillEffectMappings.js
+++ src/model/SkillEffectMappings.js
@@ -2,31 +2,13 @@
  * Created by maron on 2016/1/6.
  */
 var SkillEffectMappings = {
-    "attack_rate": {"name": "基础秒伤", "type": "rate","clickEvent":function(){}},
-    "attack_value": {"name": "秒伤值", "type": "int","clickEvent":function(){}},
-    "magma_blaster":{"name": "秒伤值1", "type": "int","clickEvent":function(){}},
-    "life_rate": {"name": "基础生命值", "type": "rate","clickEvent":function(hero,skill,equip){
-        var effects=skill.traverseSkillEffects();
-        for (var i in effects){
-            var effect= effects[i]
-            if(effect['type']==='life_rate'){
-                hero.setLife(parseInt((1+effect['value']/100)*hero.getLife()));
-                break;
-            }
-        }
-    }},
-    "life_value": {"name": "生命值", "type": "int","clickEvent":function(hero,skill,equip){
-        var effects=skill.traverseSkillEffects();
-        for (var i in effects){
-           var effect= effects[i]
-            if(effect['type']==='life_value'){
-                hero.setLife(parseInt(hero.getLife()+effect['value']));
-                break;
-            }
-        }
-    }},
-    "tap_value": {"name": "点伤值", "type": "int","clickEvent":function(){}},
-    "tap_rate": {"name": "基础点伤", "type": "rate","clickEvent":function(){}}
+    "attack_rate": {"name": "基础秒伤", "type": "rate"},
+    "attack_value": {"name": "秒伤值", "type": "int"},
+    "magma_blaster":{"name": "", "type": "int"},
+    "life_rate": {"name": "基础生命值", "type": "rate"},
+    "life_value": {"name": "生命值", "type": "int"},
+    "tap_value": {"name": "点伤值", "type": "int"},
+    "tap_rate": {"name": "基础点伤", "type": "rate"}
 }
 String.prototype.mapping = function(obj) {
     return this.replace(/{\s*\w+\s*}/gi, function(matchs) {
