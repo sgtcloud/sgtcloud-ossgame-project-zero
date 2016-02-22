8d56b0acff97f61acdd802df7aa138d35947c840
malong <malong@yoedge.com>
2016-02-22 13:14:01 +0800

    重写tip的setString

--- src/battle/BuffTips.js
+++ src/battle/BuffTips.js
@@ -1,17 +1,6 @@
 /**
- * Created by Administrator on 2016/1/21.
+ * Created by Maron on 2016/1/21.
  */
-
-//var BuffTip = cc.Node.extend({
-//    ctor: function () {
-//        this._super();
-//        var buffTip = ccs.csLoader.createNode(res.buff_tip_json);
-//        buffTip.setVisible(true);
-//        this.width = buffTip.width;
-//        this.height = buffTip.height;
-//        this.addChild(buffTip);
-//    }
-//});
 var BuffView = cc.Node.extend({
     ctor: function () {
         this._super();
@@ -20,14 +9,7 @@ var BuffView = cc.Node.extend({
         this.width = buff.width;
         this.height = buff.height;
         this.text=buff.getChildByName('root').getChildByName('text');
-        //this.text.setFontName('Helvetica');
         this.text.setFontName('微软雅黑');
-        /*
-        var children = buff.getChildren();
-        for (var i in children) {
-            children[i].setTouchEnabled && children[i].setTouchEnabled(false);
-        }*/
-        //this.text.setString('立法扩大解放了的喀什房价来说的咖啡的喀什房间里的喀什飞机拉萨的空间分达拉斯加拉塞克飞机拉萨看解放拉萨的飞机拉萨分开就');
         this.addChild(buff);
         cc.eventManager.pauseTarget(this,true);
     },
