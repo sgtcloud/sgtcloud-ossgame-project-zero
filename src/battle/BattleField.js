b8109c38d6151a98cfcf33da343e2a76bcd94aa9
peisy <peisy@yoedge.com>
2016-02-24 13:50:18 +0800

    优化。

--- src/battle/BattleField.js
+++ src/battle/BattleField.js
@@ -393,7 +393,6 @@ var BattleField = cc.Class.extend({
         scheduleOnce(this, function () {
             this.prepareBattle(stageData);
         }.bind(this), 1);
-        //同步数据到财富排行 由于资源命名问题 暂时还使用pvp_rank
         PlayerData.updateLeaderBoardScore(player.statistics.total_gold, "gold_rank");
         PlayerData.updatePlayer();
     },
