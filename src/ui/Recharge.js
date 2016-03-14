6d3e16aed3ffbd55429c6b76af54686424b1e768
peisy <peisy@yoedge.com>
2016-03-14 17:01:14 +0800

    优化充值面板

--- src/ui/Recharge.js
+++ src/ui/Recharge.js
@@ -6,8 +6,10 @@ var RechargePanel = cc.Node.extend({
     },
     initData: function () {
         var root = this.rechargeLayer.getChildByName('root');
+        root.setTouchEnabled(false);
         var vipText = root.getChildByName('text');
         this.items = root.getChildByName('box');
+        //this.items.setTouchEnabled(false);
         var closeBtn = root.getChildByName('close_btn').getChildByName('root').getChildByName('close');
         vipText.setString('敬请期待');
         for (var i = 0; i <CONSTS.chargePoints.length; i++) {
@@ -19,25 +21,26 @@ var RechargePanel = cc.Node.extend({
     },
     setElement: function (chargePoint,i) {
         var itemRoot = this.items.getChildByName('item'+(i+1)).getChildByName('root');
-        bindTouchEventListener(function(){
-            console.log('前往支付');
-            NetWork.chooseWXPay('购买钻石',1,10,function(){
-                tip.toggle('购买成功');
-            });
-        },itemRoot);
         var icon = itemRoot.getChildByName('pic');
-        icon.loadTexture('res/'+ i+'.png');
+        icon.loadTexture('res/icon/recharge/'+ i+'.png');
         var day = itemRoot.getChildByName('day');
         var moneyText = itemRoot.getChildByName('money_text');
         var gemText = itemRoot.getChildByName('gem_text');
-        moneyText.setString(chargePoint.money);
+        moneyText.setString(chargePoint.money/100);
         gemText.setString(chargePoint.amount);
         if(i > 0){
             day.setVisible(false);
         }
+        bindTouchEventListener(function(touch, event){
+            var target = event.getCurrentTarget();
+            console.log(target.getChildByName('gem_text').getString()+',前往支付');
+            /*NetWork.chooseWXPay('购买钻石',1,10,function(){
+             tip.toggle('购买成功');
+             });*/
+        },itemRoot);
     },
     openRechargePopup: function () {
-        GamePopup.openPopup(this.rechargeLayer, cc.p(330, 680),false);
+        GamePopup.openPopup(this.rechargeLayer,cc.p(335,580),false);
     },
     hiddenRechargePopup: function () {
         GamePopup.closePopup(this.rechargeLayer);
