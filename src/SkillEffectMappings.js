dd413a92731eaf989229c7a804ebbbc87c50681b
malong <malong@yoedge.com>
2016-03-17 12:48:04 +0800

    添加竞技场逻辑

--- src/SkillEffectMappings.js
+++ src/SkillEffectMappings.js
@@ -2,73 +2,74 @@
  * Created by maron on 2016/1/6.
  */
 var SkillEffectMappings = {
-    "life_value":{"name":"生命值","type":"int","fixed":0}
+    "life_value": {"name": "生命值", "type": "int", "fixed": 0}
     ,
-    "life_rate":{"name":"生命比例","type":"rate","fixed":0}
+    "life_rate": {"name": "生命比例", "type": "rate", "fixed": 0}
     ,
-    "attack_value":{"name":"攻击伤害","type":"int","fixed":0}
+    "attack_value": {"name": "攻击伤害", "type": "int", "fixed": 0}
     ,
-    "attack_rate":{"name":"攻击伤害比例","type":"rate","fixed":0}
+    "attack_rate": {"name": "攻击伤害比例", "type": "rate", "fixed": 0}
     ,
-    "tap_value":{"name":"点击伤害","type":"int","fixed":0}
+    "tap_value": {"name": "点击伤害", "type": "int", "fixed": 0}
     ,
-    "tap_rate":{"name":"点击伤害比例","type":"rate","fixed":0}
+    "tap_rate": {"name": "点击伤害比例", "type": "rate", "fixed": 0}
     ,
-    "ctr_chance_value":{"name":"暴击概率","type":"rate","fixed":0}
+    "ctr_chance_value": {"name": "暴击概率", "type": "rate", "fixed": 0}
     ,
-    "ctr_modify_value":{"name":"暴击倍率","type":"rate","fixed":0}
+    "ctr_modify_value": {"name": "暴击倍率", "type": "rate", "fixed": 0}
     ,
-    "atk_period_value":{"name":"攻击间隔","type":"int","fixed":2}
+    "atk_period_value": {"name": "攻击间隔", "type": "int", "fixed": 2}
     ,
-    "globe_life_value":{"name":"全体生命值","type":"int","fixed":0}
+    "globe_life_value": {"name": "全体生命值", "type": "int", "fixed": 0}
     ,
-    "globe_life_rate":{"name":"全体生命比例","type":"rate","fixed":0}
+    "globe_life_rate": {"name": "全体生命比例", "type": "rate", "fixed": 0}
     ,
-    "globe_ctr_modify_rate":{"name":"全体暴击倍率","type":"rate","fixed":0},
-    "globe_attack_value":{"name":"全体攻击伤害","type":"int","fixed":0}
+    "globe_ctr_modify_rate": {"name": "全体暴击倍率", "type": "rate", "fixed": 0},
+    "globe_attack_value": {"name": "全体攻击伤害", "type": "int", "fixed": 0}
     ,
-    "globe_attack_rate":{"name":"全体攻击伤害比例","type":"rate","fixed":0}
+    "globe_attack_rate": {"name": "全体攻击伤害比例", "type": "rate", "fixed": 0}
     ,
-    "globe_tap_value":{"name":"全体点击伤害","type":"int","fixed":0}
+    "globe_tap_value": {"name": "全体点击伤害", "type": "int", "fixed": 0}
     ,
-    "globe_tap_rate":{"name":"全体点击伤害比例","type":"rate","fixed":0}
+    "globe_tap_rate": {"name": "全体点击伤害比例", "type": "rate", "fixed": 0}
     ,
-    "globe_atk_period_rate":{"name":"全体攻击间隔","type":"rate","fixed":0}
+    "globe_atk_period_rate": {"name": "全体攻击间隔", "type": "rate", "fixed": 0}
     ,
-    "globe_ctr_chance_rate":{"name":"全体暴击概率","type":"rate","fixed":0}
+    "globe_ctr_chance_rate": {"name": "全体暴击概率", "type": "rate", "fixed": 0}
     ,
-    "globe_gold_rate":{"name":"金币获得比例","type":"rate","fixed":0}
+    "globe_gold_rate": {"name": "金币获得比例", "type": "rate", "fixed": 0}
     ,
-    "single_damage_once":{"name":"单体伤害输出","type":"int","fixed":0}
+    "single_damage_once": {"name": "单体伤害输出", "type": "int", "fixed": 0}
     ,
-    "multi_damage_once":{"name":"单次全体伤害","type":"int","fixed":0}
+    "multi_damage_once": {"name": "单次全体伤害", "type": "int", "fixed": 0}
     ,
-    "buff_gold_rate":{"name":"获得金币比例","type":"rate","fixed":0}
+    "buff_gold_rate": {"name": "获得金币比例", "type": "rate", "fixed": 0}
     ,
-    "multi_damage_continuous":{"name":"持续群体伤害","type":"int","fixed":0}
+    "multi_damage_continuous": {"name": "持续群体伤害", "type": "int", "fixed": 0}
     ,
-    "multi_recover_once":{"name":"全体治疗","type":"int","fixed":0}
+    "multi_recover_once": {"name": "全体治疗", "type": "int", "fixed": 0}
     ,
-    "buff_tap_rate":{"name":"提升点击伤害比例","type":"rate","fixed":0}
+    "buff_tap_rate": {"name": "提升点击伤害比例", "type": "rate", "fixed": 0}
     ,
-    "buff_attack_rate":{"name":"提升攻击伤害比例","type":"rate","fixed":0}
+    "buff_attack_rate": {"name": "提升攻击伤害比例", "type": "rate", "fixed": 0}
     ,
-    "buff_atk_period_rate":{"name":"缩短攻击间隔比例","type":"rate","fixed":0}
+    "buff_atk_period_rate": {"name": "缩短攻击间隔比例", "type": "rate", "fixed": 0}
     ,
-    "buff_ctr_chance_rate":{"name":"提升暴击概率比例","type":"rate","fixed":0}
+    "buff_ctr_chance_rate": {"name": "提升暴击概率比例", "type": "rate", "fixed": 0}
     ,
-    "buff_ctr_modify_rate":{"name":"提升暴击倍率比例","type":"rate","fixed":0}
+    "buff_ctr_modify_rate": {"name": "提升暴击倍率比例", "type": "rate", "fixed": 0},
+    "fs_damage_once": {"name": "fs_damage_once", type: "int", "fixed": 0}
 }
-String.prototype.mapping = function(obj) {
-    return this.replace(/{\s*\w+\s*}/gi, function(matchs) {
-        var match=matchs.replace(/{\s*|\s*}/g, "")
+String.prototype.mapping = function (obj) {
+    return this.replace(/{\s*\w+\s*}/gi, function (matchs) {
+        var match = matchs.replace(/{\s*|\s*}/g, "")
         var returns;
-        if (match.indexOf('_')>-1){
-            var arr=match.split("_");
-            if(arr.length>1){
+        if (match.indexOf('_') > -1) {
+            var arr = match.split("_");
+            if (arr.length > 1) {
                 returns = obj[arr[0]][arr[1]];
             }
-        }else {
+        } else {
             returns = obj[match];
         }
         return (returns + "") == "undefined" ? "" : returns;
--- src/ui/ArenaPanel.js
+++ src/ui/ArenaPanel.js
@@ -2,23 +2,74 @@
  * 竞技场面板
  * Created by maron on 2016/3/7.
  */
-var ArenaPanel=BattleMenu.extend({
-    ctor:function(tabPanel){
-        this._super(tabPanel,res.pvp_layer_json);
-        this.panel=this.root;//cc.csLoader.createNode(res.pvp_layer_json).getChildByName('root');
-        this.changeLayer=this.panel.getChildByName('change_btn');
-        this.changeBtn=this.changeLayer.getChildByName('change');
-        this.changeText=this.changeLayer.getChildByName('change_text');
-        this.buyLayer=this.panel.getChildByName('buy_btn');
-        this.buyBtn=this.buyLayer.getChildByName('buy');
-        this.buyText=this.buyLayer.getChildByName('buy_text');
-        this.recordLayer=this.panel.getChildByName('record_btn');
-        this.recordBtn=this.buyLayer.getChildByName('record');
-        this.recordText=this.buyLayer.getChildByName('record_text');
-        this.surplusText=this.panel.getChildByName('surplus_text');//剩余次数
-        this.surplusNum=this.panel.getChildByName('surplus_num');//剩余次数
-        this.opponentBox=this.panel.getChildByName('opponent_box');//挑战者列表
+var ArenaPanel = BattleMenu.extend({
+    ctor: function (tabPanel) {
+        this._super(tabPanel, res.pvp_layer_json);
+        this.panel = this.root;//cc.csLoader.createNode(res.pvp_layer_json).getChildByName('root');
+        this.changeLayer = this.panel.getChildByName('change_btn');
+        this.changeBtn = this.changeLayer.getChildByName('change');
+        this.changeText = this.changeLayer.getChildByName('change_text');
+        this.buyLayer = this.panel.getChildByName('buy_btn');
+        this.buyBtn = this.buyLayer.getChildByName('buy');
+        this.buyText = this.buyLayer.getChildByName('buy_text');
+        this.recordLayer = this.panel.getChildByName('record_btn');
+        this.recordBtn = this.buyLayer.getChildByName('record');
+        this.recordText = this.buyLayer.getChildByName('record_text');
+        this.surplusText = this.panel.getChildByName('surplus_text');//剩余次数
+        this.surplusNum = this.panel.getChildByName('surplus_num');//剩余次数
+        this.opponentBox = this.panel.getChildByName('opponent_box');//挑战者列表
         //setFont(this.changeText,this.buyText,this.recordText,this.surplusNum,this.surplusText);
         //setColor(this.changeText,this.buyText,this.recordText,this.surplusNum,this.surplusText);
+        this._arenakey='test-arena';
+        this.pullData();
+    }, pushItem: function () {
+
+    }, pullData: function () {
+        this._arenaService = sgt.getCustomService('arena', ["getPlayersByIndex", "addToEnd", "exchangeIndex", "checkInArena"]);
+        this._arenaService.addToEnd(this._arenakey, player.id, function (result, data) {
+            console.log(data);
+            if (result) {
+                var items;
+                if (data < 5) {
+                    items=this._indexInner5(data);
+                } else if (data <= 30) {
+                    items=this._indexInner30(data);
+                } else {
+                    items=this._indexOuter30(data);
+                }
+                this._arenaService.getPlayersByIndex(items,this._arenakey,function(){
+
+                }.bind(this));
+            }
+        }.bind(this));
+    }, _indexInner5: function (index) {
+        console.log('player index:[' + index + '] inner 5');
+        var items = [];
+        for (var i = 0; i < 5; i++) {
+            if (index != i) {
+                items.push(i);
+            }
+        }
+        items.sort();
+        return items;
+    }, _indexInner30: function (index) {
+        console.log('player index:[' + index + '] inner 30');
+       return this._random(index,1);
+    }, _indexOuter30: function (index) {
+        console.log('player index:[' + index + '] outer 30');
+        return this._random(index,5);
+    }, _exchangeIndex: function (targetPlayerId) {
+        console.log('exchange index with player:[' + targetPlayerId + ']');
+    },_random:function(index,base){
+        var items = [];
+        for (var i = 0; i < 4; i++) {
+            var _index=index/(4-i);
+            if(i===3){
+                _index=index/1.05;
+            }
+            items.push(getRandomInt(_index-base,_index+base));
+        }
+        items.sort();
+        return items;
     }
 });
