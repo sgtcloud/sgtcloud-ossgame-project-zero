2ec0034aa4b8c471540de1f37897ab40d3193b6e
peisy <peisy@yoedge.com>
2016-03-21 14:56:24 +0800

    优化弹出框蒙版。 增加alert和confirm 两种Popup(BasicPopup、ComplexPopup). 优化微信登陆。

--- src/apps.js
+++ src/apps.js
@@ -421,6 +421,13 @@ function is_weixin() {
     }
 }
 
+//获取url中的参数
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg); //匹配目标参数
+    if (r !== null) return unescape(r[2]);
+}
+
 var tipTemplate;
 function showCover() {
     var scene = ccs.csLoader.createNode(res.cover_scene_json);
